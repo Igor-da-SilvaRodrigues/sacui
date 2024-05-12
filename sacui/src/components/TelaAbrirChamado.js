e59e8660cad6f8b67be65336cee7f4dd1b5d1cd1
Igor Rodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2024-05-12 17:29:05 -0300

    Finalizar tela de abertura de chamado
    O contém um formulário e um botão de inserção.
    Aceita como argumentos os dados do usuário, a url da api e o token de sessão
    Contém callbacks para o sucesso ou fracasso da abertura para o tratamento desses eventos pelo componente pai.
    
    Criado hook usePost para operações POST
    
    Criado hook useCreateChamado para abrir um chamado na API.

--- sacui/src/components/TelaAbrirChamado.js
+++ sacui/src/components/TelaAbrirChamado.js
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import {
-    MDBContainer, MDBRow, MDBCol,
-    MDBTable, MDBTableHead, MDBTableBody,
-    MDBBtn, MDBCheckbox, MDBIcon,
-    MDBPagination, MDBPaginationItem, MDBPaginationLink, MDBTextArea
+    MDBContainer,
+    MDBBtn, MDBTextArea
 } from 'mdb-react-ui-kit';
 import { useGetAllTipoChamado } from '../hooks/useGetAllTipoChamado';
+import { useCreateChamado } from '../hooks/useCreateChamado';
 
 /**
  * Representa a tela de abertura de chamados
  * @param {object} user dados do usuário, contendo nome, matrícula e status de admin.
  * @param {string} url A url da api.
  * @param {string} token o token da sessão. 
+ * @param {*} onAbrirChamado um callback que será chamado quando um chamado for aberto com sucesso, pode ser usado caso exista alguma operação do componente pai que dependa do sucesso da abertura do chamado, como por exemplo, o desmontamento/fechamento deste componente
+ * @param {*} onError um callback que será chamado quando algo der errado na abertura, e caso o componente pai deseje tratar esse erro de alguma forma. O callback recebe um objeto contendo os erros como argumento no seguinte formato:{errorCreateChamado, errorTipoChamado}.
  * @returns 
  */
-const TelaAbrirChamado = ({ user, url, token }) => {
-    const { isLoading, isError, error, statusCode, tipos } = useGetAllTipoChamado(`${url}/tipochamado?`, token)
+const TelaAbrirChamado = ({ user, url, token, onAbrirChamado, onError}) => {
+    const { isLoading: isLoadingTipoChamado, isError: isErrorTipoChamado, error: errorTipoChamado, statusCode: statusCodeTipoChamado, tipos } = useGetAllTipoChamado(`${url}/tipochamado?`, token)
     const [selectedTipo, setSelectedTipo] = useState(null)
     const [selectedMotivo, setSelectedMotivo] = useState(null)
-
+    const [justificativa, setJustificativa] = useState("")
+    const {commit, isLoading: isLoadingCreateChamado, isError: isErrorCreateChamado, error: errorCreateChamado, statusCode: statusCodeCreateChamado, chamado} = useCreateChamado(`${url}/chamado?`, token)
 
     const handleSelectChamadoChange = (e)=>{
         setSelectedTipo(tipos.find(tipo => tipo["tipo"] === e.target.value))
@@ -26,8 +28,32 @@ const TelaAbrirChamado = ({ user, url, token }) => {
 
     const handleSelectMotivoChange = (e)=>{
         setSelectedMotivo(e.target.value)
-        console.log(`Choosing ${e.target.value}`)
     }
+    const handleJustificativaChange = (e)=>{
+        setJustificativa(e.target.value)
+    }
+
+    //abrir chamado
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        commit({
+            "idTipoChamado": selectedTipo["tipo"],
+            "idMotivo": selectedMotivo,
+            "justificativa": justificativa,
+            "dataAbertura": "2012-12-24",//a data vai ser sobrescrita pela data atual no backend
+            "idDiscente": user["matricula"]
+          })
+    }
+
+    //lógica para acionar os callbacks
+    useEffect(()=>{
+        //caso algum erro tenha ocorrido, não faça nada e chame o callback de erro
+        if(isErrorCreateChamado || isErrorTipoChamado){
+            onError != undefined && onError({errorCreateChamado, errorTipoChamado}); // execute onError apenas se ele for fornecido
+        }else if(chamado != null && statusCodeCreateChamado === 200){//chamado ainda pode ser nulo mesmo que não haja erro, evitando chamar callback neste caso.  
+            onAbrirChamado != undefined && onAbrirChamado();//execute onAbrirChamado apenas se ele for fornecido
+        }
+    }, [chamado, statusCodeCreateChamado, isErrorCreateChamado, isErrorTipoChamado])
 
     return (
         <MDBContainer className='mt-5 border rounded col-6 p-5 pb-4'>
@@ -35,7 +61,7 @@ const TelaAbrirChamado = ({ user, url, token }) => {
             <h3 className='mb-5'>Abrir chamado</h3>
 
 
-            <form action="#" className='d-grid gap-3'>
+            <form action="#" onSubmit={handleSubmit} className='d-grid gap-3'>
                 <div className='d-flex'>
                     Chamado
                     <select defaultValue={"default"} className=' ms-3 border rounded flex-fill bg-white text-dark' onChange={handleSelectChamadoChange}>
@@ -54,10 +80,10 @@ const TelaAbrirChamado = ({ user, url, token }) => {
                         })}
                     </select>
                 </div>
-                <MDBTextArea label='Justificativa' rows={4}></MDBTextArea>
-                <MDBBtn>Enviar</MDBBtn>
+                <MDBTextArea label='Justificativa' rows={4} onChange={handleJustificativaChange}></MDBTextArea>
+                <MDBBtn type='submit'>Enviar</MDBBtn>
             </form>
-            <span style={{ visibility: isLoading ? "visible" : "hidden" }} className=''>Carregando...</span>
+            <span style={{ visibility: isLoadingTipoChamado || isLoadingCreateChamado ? "visible" : "hidden" }} className=''>Carregando...</span>
         </MDBContainer>
     )
 }
